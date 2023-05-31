f50912f439c6d2fc381fbff09ea88c3ace3dc112
Carlos Eduardo Ferreira <mail@carlosferreira.dev>
2023-05-30 21:24:25 -0300

    .

--- pyside6/aula01.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtWidgets import QApplication, QWidget
-import sys
-
-app = QApplication(sys.argv)
-
-janela = QWidget()
-janela.show()
-
-app.exec()
--- pyside6/aula02 QMainWindow.py
+++
@@ -1,8 +0,0 @@
-from PySide6.QtWidgets import QApplication, QMainWindow
-import sys
-
-app = QApplication(sys.argv)
-jan = QMainWindow()
-jan.show()
-
-app.exec()
